4907174c382e9cc894b69d10d0510f01f32ee7ef
Vlad Lopalo <vlad.lopalo@gmail.com>
2019-12-08 17:06:30 +0200

    S-expression parser

--- lib/object.ml
+++ lib/object.ml
@@ -1,6 +1,14 @@
+let format = Printf.sprintf
+
 type symbol = Symbol of string [@@unboxed]
 
-(* TODO: PairWithMeta, SymWithMeta, HashTable, Record *)
+type position =
+  { line_number : int;
+    column_number : int }
+
+type meta = position
+
+(* TODO: HashTable, Record *)
 type t =
   | Null
   | Bool of bool
@@ -9,7 +17,69 @@ type t =
   | Char of char
   | Str of string
   | Sym of symbol
+  | Pair of pair
   | Vec of t array
-  | Pair of
-      { mutable car : t;
-        mutable cdr : t }
+  | SymM of symbol * meta
+  | PairM of pair * meta
+
+and pair =
+  { mutable car : t;
+    mutable cdr : t }
+
+let cons car cdr = {car; cdr}
+
+let is_proper_pair = function
+  | Null -> true
+  | Pair {cdr; _}
+  | PairM ({cdr; _}, _) -> (
+    match cdr with
+    | Null
+    | Pair _
+    | PairM _ ->
+        true
+    | _ -> false)
+  | _ -> false
+
+let pairs_of_list objects =
+  List.fold_right (fun head tail -> Pair (cons head tail)) objects Null
+
+let rec to_string = function
+  | Null -> "()"
+  | Bool b -> string_of_bool b
+  | Int i -> string_of_int i
+  | Float f -> string_of_float f
+  | Char c -> (
+    match c with
+    | '\n' -> "\\newline"
+    | ' ' -> "\\space"
+    | _ -> "\\" ^ Char.escaped c)
+  | Str s -> "\"" ^ s ^ "\""
+  | Sym (Symbol s)
+  | SymM (Symbol s, _) ->
+      s
+  | (Pair _ as p)
+  | (PairM _ as p) ->
+      "(" ^ pair_to_string p ^ ")"
+  | Vec _ -> "(#vector)"
+
+and pair_to_string = function
+  | Pair {car; cdr = Null}
+  | PairM ({car; cdr = Null}, _) ->
+      to_string car
+  | (Pair {car; cdr} as p)
+  | (PairM ({car; cdr}, _) as p) ->
+      if is_proper_pair p then to_string car ^ " " ^ pair_to_string cdr
+      else format "#pair %s %s" (to_string car) (to_string cdr)
+  | o -> to_string o
+
+let bad_arg func_name obj = invalid_arg @@ func_name ^ ": " ^ to_string obj
+
+let set_meta obj meta =
+  match obj with
+  | Sym s
+  | SymM (s, _) ->
+      SymM (s, meta)
+  | Pair p
+  | PairM (p, _) ->
+      PairM (p, meta)
+  | o -> bad_arg "set_meta" o
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,7 +1,8 @@
+module O = Option
 module S = String
 module L = List
 module LL = ListLabels
-module O = Object
+module Obj = Object
 
 let format = Printf.sprintf
 
@@ -11,19 +12,20 @@ type state =
     line_number : int;
     column_number : int }
 
-type position =
-  { line_number : int;
-    column_number : int }
+type error = ParseError of state * string
 
-type error = ParseError of string * state
+type 'a t =
+  { parse :
+      'b. state -> (state -> 'a -> ('b, error) result) -> ('b, error) result }
+[@@unboxed]
 
-type ('a, 'b) parser =
-  ('a -> state -> ('b, error) result) -> state -> ('b, error) result
+let return value = {parse = (fun state cont -> cont state value)}
 
-let return value cont state = cont value state
-
-let bind parser f cont state =
-  parser (fun value state' -> (f value) cont state') state
+let bind parser f =
+  { parse =
+      (fun state cont ->
+        parser.parse state (fun state' value -> (f value).parse state' cont))
+  }
 
 let ( let* ) = bind
 
@@ -31,9 +33,14 @@ let fmap f parser =
   let* r = parser in
   return (f r)
 
-let error msg state = Error (ParseError (msg, state))
+let error state msg = Error (ParseError (state, msg))
 
-let failure msg _ state = error msg state
+let failure msg = {parse = (fun state _ -> error state msg)}
+
+let current_position =
+  { parse =
+      (fun ({line_number; column_number; _} as state) cont ->
+        cont state ({line_number; column_number} : Obj.position)) }
 
 let chars_of_string string = L.init (S.length string) (S.get string)
 
@@ -42,26 +49,31 @@ let string_of_chars chars =
   L.iter (Buffer.add_char buf) chars;
   Buffer.contents buf
 
-let any cont ({input; offset; line_number; column_number} as state) =
-  if S.length input = 0 then error "End of input is reached" state
-  else
-    let char = input.[0] in
-    let input' = S.sub input 1 (S.length input - 1) in
-    let line_number', column_number' =
-      if char = '\n' then (succ line_number, 0)
-      else (line_number, succ column_number)
-    in
-    cont char
-      { input = input';
-        offset = succ offset;
-        line_number = line_number';
-        column_number = column_number' }
+let any =
+  { parse =
+      (fun ({input; offset; line_number; column_number} as state) cont ->
+        if S.length input = 0 then error state "End of input is reached"
+        else
+          let char = input.[0] in
+          let input' = S.sub input 1 (S.length input - 1) in
+          let line_number', column_number' =
+            if char = '\n' then (succ line_number, 0)
+            else (line_number, succ column_number)
+          in
+          cont
+            { input = input';
+              offset = succ offset;
+              line_number = line_number';
+              column_number = column_number' }
+            char) }
 
 let satisfy ?(pred_name = "unspecified") pred =
-  (fun value ->
-    if pred value then return value
-    else format "Predicate '%s' is not satisfied" pred_name |> failure)
-  |> bind any
+  bind any (fun char ->
+      if pred char then return char
+      else format "Predicate '%s' is not satisfied" pred_name |> failure)
+
+let unexpected_char =
+  bind any (fun char -> format "Unexpected character '%c'" char |> failure)
 
 let one_of chars =
   LL.mem ~set:chars |> satisfy ~pred_name:("one of: " ^ string_of_chars chars)
@@ -69,6 +81,10 @@ let one_of chars =
 let none_of chars =
   LL.mem ~set:chars |> Fun.negate
   |> satisfy ~pred_name:("none of: " ^ string_of_chars chars)
+
+let character c = one_of [c]
+
+let dot = character '.'
 
 let letter =
   (function
@@ -91,49 +107,159 @@ let and_then ~combine parser parser' =
   let* r' = parser' in
   combine r r' |> return
 
-let or_else parser parser' cont state =
-  match parser cont state with
-  | Ok _ as r -> r
-  | _ -> parser' cont state
+let first x _ = x
 
-let optional parser = [] |> return |> or_else parser
+let second _ x = x
 
-let rec many parser = parser |> plus |> optional
+let or_else parser parser' =
+  { parse =
+      (fun state cont ->
+        match parser.parse state cont with
+        | Ok _ as r -> r
+        | Error (ParseError (s, _)) as r -> (
+          match parser'.parse state cont with
+          | Ok _ as r -> r
+          | Error (ParseError (s', _)) as r' ->
+              if s.offset > s'.offset then r else r')) }
+
+let optional parser = or_else (fmap O.some parser) (return O.none)
+
+let rec many parser = or_else (plus parser) (return [])
 
 and plus parser =
   let* x = parser in
   let* xs = many parser in
   return (x :: xs)
 
+let rec separated_many ~sep parser =
+  or_else (separated_plus ~sep parser) (return [])
+
+and separated_plus ~sep parser =
+  let* _ = optional sep in
+  let* x = parser in
+  let* opt_sep = optional sep in
+  match opt_sep with
+  | Some _ -> fmap (L.cons x) (separated_many ~sep parser)
+  | None -> return [x]
+
 let alphanumeric = or_else letter digit
 
 let spaces = plus space
 
 let string s =
-  let char_parsers = s |> chars_of_string |> L.map (fun c -> one_of [c]) in
+  let char_parsers = chars_of_string s |> L.map character in
   L.fold_right (and_then ~combine:L.cons) char_parsers (return [])
   |> fmap string_of_chars
 
-let end_of_input cont ({input; _} as state) =
-  if S.length input = 0 then cont () state
-  else error "End of input is not reached" state
+let end_of_input =
+  { parse =
+      (fun ({input; _} as state) cont ->
+        if S.length input = 0 then cont state ()
+        else error state "End of input is not reached") }
 
 let parse_all parser input =
-  (and_then ~combine:(fun x _ -> x) parser end_of_input)
-    (fun x state -> Ok (x, state))
-    {input; offset = 0; line_number = 0; column_number = 0}
+  (and_then ~combine:first parser end_of_input).parse
+    {input; offset = 0; line_number = 0; column_number = 0} (fun state x ->
+      Ok (state, x))
+
+let null = fmap (Fun.const Obj.Null) (string "()")
+
+let boolean =
+  let* s = or_else (string "true") (string "false") in
+  return @@ Obj.Bool (s = "true")
+
+let sign minus =
+  let* opt_sign = optional @@ one_of ['+'; '-'] in
+  (match opt_sign with
+  | Some '-' -> minus
+  | _ -> Fun.id)
+  |> return
+
+let integer =
+  let* sign = sign ( ~- ) in
+  let obj i = Obj.Int i in
+  let* ds = plus digit in
+  string_of_chars ds |> int_of_string |> sign |> obj |> return
+
+let floating =
+  let* sign = sign ( ~-. ) in
+  let obj i = Obj.Float i in
+  let* ds = plus digit in
+  let* dot = dot in
+  let* ds' = plus digit in
+  let ds'' = ds @ (dot :: ds') in
+  string_of_chars ds'' |> float_of_string |> sign |> obj |> return
+
+let char_literal =
+  let space = fmap (Fun.const ' ') (string "\\space") in
+  let newline = fmap (Fun.const '\n') (string "\\newline") in
+  let c = and_then ~combine:second (character '\\') any in
+  fmap (fun x -> Obj.Char x) (or_else space @@ or_else newline c)
+
+let string_literal =
+  let d_char = '"' in
+  let delimeter = character d_char in
+  let backslash = fmap (Fun.const '\\') (string "\\\\") in
+  let double_quote = fmap (Fun.const '"') (string "\\\"") in
+  let* _ = delimeter in
+  let* chars =
+    many @@ or_else backslash @@ or_else double_quote @@ none_of [d_char]
+  in
+  let* _ = delimeter in
+  return @@ Obj.Str (string_of_chars chars)
+
+let symbol =
+  let init =
+    or_else letter
+    @@ one_of
+         ['!'; '$'; '%'; '&'; '*'; '/'; ':'; '<'; '='; '>'; '?'; '~'; '_'; '^']
+  in
+  let subs = many @@ or_else init @@ or_else digit @@ one_of ['+'; '-'; '.'] in
+  let* c = init in
+  let* pos = current_position in
+  let* cs = subs in
+  let obj s = Obj.SymM (Obj.Symbol s, pos) in
+  c :: cs |> string_of_chars |> obj |> return
+
+let rec list_literal () =
+  let* d_char = one_of ['('; '['] in
+  let* pos = current_position in
+  let* content = separated_plus ~sep:spaces (lisp ()) in
+  let* _ = character (if d_char = '(' then ')' else ']') in
+  return @@ Obj.set_meta (Obj.pairs_of_list content) pos
+
+and lisp () =
+  L.fold_left or_else null
+    [ boolean;
+      integer;
+      floating;
+      char_literal;
+      string_literal;
+      symbol;
+      list_literal ();
+      unexpected_char ]
+
+let lisp_parser = lisp () |> separated_many ~sep:spaces |> parse_all
+
+let parse_lisp str =
+  lisp_parser str
+  |> Result.map (fun (x, y) -> (x, y, Obj.to_string @@ Obj.pairs_of_list y))
 
 (* For testing *)
 
-let word =
-  let* chars = plus letter in
-  let* _ = optional spaces in
-  return @@ string_of_chars chars
+let sentences =
+  let word = fmap string_of_chars (plus alphanumeric) in
+  let sentence = separated_plus ~sep:spaces word in
+  separated_many ~sep:dot sentence
 
-let words =
-  let* w = string "Jelly" in
-  let* _ = spaces in
-  let* ws = many word in
-  return @@ (w :: ws)
+let sentence_parser = parse_all sentences
 
-let words_parser = parse_all words
+let test_sentences () =
+  sentence_parser
+    " Jelly is\n\
+    \    a  programming language\n\
+     , so is Python. Sentence 2.\n\
+     Sentence   \n\
+    \ xxxx\n\
+    \      3.  Sentence 4 zzzzz \n\
+     . Sentence 5  "
